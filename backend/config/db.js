84701325528e467ca9137b434e14f1ce145fa17c
Testgithubrit504 <aryanspawar05@gmail.com>
2025-06-10 18:00:14 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,16 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      process.env.MONGO_URL
+      process.env.MONGO_URL, // Make sure your Render env variable is also MONGO_URL
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true, // Ensure SSL is used (important for Atlas/Render)
+      }
     )
-    .then(() =>console.log("DB Connected"));
+    .then(() => console.log("DB Connected"))
+    .catch((err) => {
+      console.error("DB Connection Error:", err);
+      process.exit(1);
+    });
 };
